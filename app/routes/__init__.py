961336f154b60d561374988a2645a9fec86ff908
Murilo Martins <murilolmartins1@gmail.com>
2022-04-22 12:57:22 -0300

    feat: patient model

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, Flask
 from .user_route import bp as bp_user
+from .patient_route import bp as bp_patient
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
 
 
 def init_app(app: Flask):
     bp_api.register_blueprint(bp_user)
+    bp_api.register_blueprint(bp_patient)
 
     app.register_blueprint(bp_api)
