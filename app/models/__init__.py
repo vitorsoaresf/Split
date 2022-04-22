5ed8b0b4bcb55a54e81bc878419a71946da7c1f6
Murilo Martins <murilolmartins1@gmail.com>
2022-04-22 13:20:06 -0300

    fix: models imports

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .allergy_model import Allergy
+from .category_model import Category
+from .data_model import Data
+from .patient_allergie_table import patients_allergies
+from .patient_model import Patient
+from .user_model import User
+# from .users_workespaces_table import users_workspaces
