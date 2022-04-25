6cf5547b9e4386f85824836a3e67d43c86bc25af
Daniel <ddan.mm.16@gmail.com>
2022-04-25 10:14:50 -0300

    feat: create workspace model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,5 @@ from .data_model import Data
 from .patient_allergie_table import patients_allergies
 from .patient_model import Patient
 from .user_model import User
+from .workspace_model import Workspace
 # from .users_workespaces_table import users_workspaces
---
+++ app/models/workspace_model.py
@@ -0,0 +1,38 @@
+from app.configs.database import db
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from marshmallow import Schema, fields
+
+
+class Workspace(db.Model):
+    """Workspace class.
+
+    This class represents some information about workspaces.
+
+    Attributes:
+        workspace_id: A unique integer value identifying the data.
+        name: A string value with workspace name.
+        local: A string field with workspace address.
+    """
+
+    __tablename__ = "workspaces"
+
+    workspace_id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    local = Column(String(256))
+
+
+class WorkspaceSchema(Schema):
+    """Workspace schema.
+
+    This class represents the schema of the workspace class.
+    Will check values and validate them.
+
+    Attributes:
+        workspace_id: A unique integer value identifying the data.
+        name: A string value with workspace name.
+        local: A string field with workspace address.
+    """
+
+    workspace_id = fields.Integer()
+    name = fields.String()
+    local = fields.String()
