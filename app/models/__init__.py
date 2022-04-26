4e278c92e03e0ea5b5ede1ae3d8dec0da601b3d6
vitorsoaresf <vitorsoaresf@gmail.com>
2022-04-26 11:31:57 -0300

    feature-address-model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ from .patient_allergie_table import patients_allergies
 from .patient_model import Patient
 from .user_model import User
 from .workspace_model import Workspace
+from .address_model import Address
 from .categories_workspaces import categories_workspaces
 from .users_workespaces_table import users_workspaces
