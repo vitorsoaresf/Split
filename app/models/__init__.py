3e2b0954296aced5ed1ac67dce1344c3302232f8
Márcio Alves <alvesmarcio@hotmail.com>
2022-04-26 11:07:42 -0300

    feature: create data controller

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .allergy_model import Allergy
 from .category_model import Category
-from .data_model import Data
+from .data_model import Data, DataSchema
 from .patient_allergie_table import patients_allergies
 from .patient_model import Patient
 from .user_model import User
