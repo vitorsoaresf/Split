326d533ca9fa21d72007faeda2737933031c5783
Daniel <ddan.mm.16@gmail.com>
2022-04-26 13:58:15 -0300

    feat: get all workspaces and especific workspace in controllers

--- app/controllers/workspace_controller.py
+++ app/controllers/workspace_controller.py
@@ -20,3 +20,20 @@ def create_workspace():
     response = schema.dump(workspace)
 
     return response, HTTPStatus.CREATED
+
+
+def get_workspaces():
+    workspaces = Workspace.query.all()
+
+    return WorkspaceSchema(many=True).dump(workspaces), HTTPStatus.OK
+
+
+def get_specific_workspace(id: int):
+    workspace = Workspace.query.get(id)
+
+    if not workspace:
+        return {"msg": "Workspace not Found"}, HTTPStatus.NOT_FOUND
+
+    return WorkspaceSchema().dump(workspace), HTTPStatus.OK
+
+
