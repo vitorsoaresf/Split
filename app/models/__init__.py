94dab95fedf72e31d3d95db705810c0c0cd2f0a5
Márcio Alves <alvesmarcio@hotmail.com>
2022-04-28 15:35:53 -0300

    fix: some database configurations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,10 @@
+from .address_model import Address, AddressSchema
 from .allergy_model import Allergy, AllergySchema
+from .categories_workspaces_table import categories_workspaces
 from .category_model import Category, CategorySchema
 from .data_model import Data, DataSchema
 from .patient_allergie_table import patients_allergies
 from .patient_model import Patient, PatientSchema
 from .user_model import User, UserSchema
-from .workspace_model import Workspace, WorkspaceSchema
-from .address_model import Address, AddressSchema
-from .categories_workspaces_table import categories_workspaces
 from .users_workspaces_table import users_workspaces
+from .workspace_model import Workspace, WorkspaceSchema
