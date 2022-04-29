290b4b09d77011f5049ffbafd8ed4a3baf6c9c77
Diego <diegohpsousa@gmail.com>
2022-04-29 09:44:50 -0300

    feature: implement marshmallow

--- app/models/tag_model.py
+++ app/models/tag_model.py
@@ -1,22 +1,20 @@
 from app.configs.database import db
 from sqlalchemy import ForeignKey, Integer, String, Column, Boolean
-# from marshmallow import Schema, fields, validate, validates
+from marshmallow import Schema, fields
 
 class Tag(db.Model):
 
     __tablename__ = "tags"
 
+    alert_tag = Column(Boolean)
     tag_id = Column(Integer, primary_key=True)
     tag = Column(String, nullable=False)
-    data_id = Column(Integer, ForeignKey("datas.data_id"), nullable=False)
+    data_id = Column(Integer, ForeignKey("datas.data_id"))
     patient_id = Column(Integer, ForeignKey("patients.patient_id"))
-    alert_tag = Column(Boolean)
 
-# class TagSchema(Schema):
+class TagSchema(Schema):
 
-#     tag = fields.String()
-#     alert_tag = fields.Boolean()
+    tag = fields.String()
+    alert_tag = fields.Boolean()
 
-#     @validates("tag")
-#     def valida_n_char():
         
